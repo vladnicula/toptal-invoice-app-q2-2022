3d4c0783a669c3608ece758dda0f255dca6f08f4
Vlad Nicula <vladnicula@gmail.com>
2022-05-10 18:35:13 +0300

    Session 3 Progress

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button'
+
+export default function AboutPage () {
+    return (
+        <div>
+            <Button variant="contained">Hello</Button>
+        </div>
+    )
+}
